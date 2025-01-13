8821d793fa81d967cb9263e785ed6f930a55fd2b
Gustavo <gustavoburgerf@gmail.com>
2025-01-13 10:54:13 -0300

    criando o pesquisa tarefas

---
+++ src/containers/ListaDeTarefas/index.tsx
@@ -0,0 +1,41 @@
+import { useSelector } from 'react-redux'
+
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+
+import { RootReducer } from '../../store'
+
+const ListaDetarefas = () => {
+  const { itens } = useSelector((state: RootReducer) => state.tarefas)
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+
+  const filtraTarefas = () => {
+    return itens.filter(
+      (itens) =>
+        itens.titulo.toLowerCase().search(termo.toLocaleLowerCase()) >= 0
+    )
+  }
+
+  return (
+    <Container>
+      <p>
+        2 tarefas marcadas como: &quot;categotia&ldquo; e &quot;{termo}&ldquo;
+      </p>
+      <ul>
+        {filtraTarefas().map((t) => (
+          <li key={t.titulo}>
+            <Tarefa
+              id={t.id}
+              descricao={t.descricao}
+              titulo={t.titulo}
+              status={t.status}
+              prioridade={t.prioridade}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
+
+export default ListaDetarefas
---
+++ src/store/reducers/tarefas.ts
@@ -0,0 +1,52 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import * as enums from '../../utils/enums/tarefa'
+import Tarefa from '../../models/Tarefa'
+import { Titulo } from '../../components/Tarefa/styles'
+
+type TarefasState = {
+  itens: Tarefa[]
+}
+
+const initialState: TarefasState = {
+  itens: [
+    {
+      id: 1,
+      descricao: 'Estudar JavaScript revendo o exercicio do módulo 7',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.CONCLUIDA,
+      titulo: 'Estudar JavaScript'
+    },
+    {
+      id: 2,
+      descricao: 'Estudar material de apoio',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.PENDENTE,
+      titulo: 'Estudar TypeScript'
+    },
+    {
+      id: 3,
+      descricao: 'Praticar a construção de uma landing page',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.CONCLUIDA,
+      titulo: 'Estudar Bootstrap'
+    }
+  ]
+}
+
+const tarefaSlice = createSlice({
+  name: 'tarefas',
+  initialState,
+  reducers: {
+    remover: (state, action: PayloadAction<number>) => {
+      state.itens = state.itens.filter((Tarefa) => Tarefa.id !== action.payload)
+    },
+    editar: (state, action: PayloadAction<Tarefa>) => {
+      const indexDaTarefa = state.itens.findIndex(
+        (t) => t.id === action.payload.id
+      )
+    }
+  }
+})
+
+export const { remover, editar } = tarefaSlice.actions
+export default tarefaSlice.reducer
